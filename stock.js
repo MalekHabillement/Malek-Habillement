224b091c928ed2b1a74e1040d05127cb6e71717a
MalekHabillement <redaalg@gmail.com>
2026-07-17 13:19:28 +0200

    Update stock.js
    Add stock structure

--- stock.js
+++ stock.js
@@ -1,1 +1,43 @@
+const STOCK = {
+  noir: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
 
+  beige: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
+
+  marron: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
+
+  bordeaux: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
+
+  violet: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
+
+  bleu: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  },
+
+  vert: {
+    L: 0,
+    XL: 0,
+    XXL: 0
+  }
+};
